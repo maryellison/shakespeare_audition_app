90b0732bd08ae18f7c16672d531080bba6a6a784
Mary Ellison Barlow <maryellisonbarlow@gmail.com>
2022-11-19 23:47:17 -0500

    Style changes to css and added basic functions

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
 This is my readme file
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title>Monologues</title>
+        <link rel="stylesheet" href="./index.css"/>
+        <script defer src="src/index.js"></script>
+    </head>
+      <body>
+          <h1>Plays</h1>
+          <button id="plays">All Plays</button>
+          <hr>
+          <br>
+          <div id="main">
+              <ul id="play-list">
+
+
+              </ul>
+              <div id="info"></div>
+          </div>
+          <footer class="branding">
+            <small>&copy; 2022 <strong>ShakespeareAuditionApp</strong></small>
+          </footer>
+      </body>
+</html>
---
+++ src/index.js
@@ -0,0 +1,94 @@
+let addMonologue = false;
+
+document.addEventListener("DOMContentLoaded", () => {
+ const addBtn = document.querySelector("#new-monologue-btn");
+ const monologueFormContainer = document.querySelector(".container");
+ const form = document.querySelector(".add-monologue-form")
+
+ form.addEventListener("sumbit", addNewMonologue)
+
+//  document.addEventListener("click", (e) => {
+//    if(e.target.matches(".like-btn")) {
+//      updatesLikes(e)
+//    }
+//  })
+
+  addBtn.addEventListener("click", () => {
+    // hide & seek with the form
+    addMonologue = !addMonologue;
+    if (addMonologue) {
+      monologueFormContainer.style.display = "block";
+    } else {
+      monologueFormContainer.style.display = "none";
+    }
+  });
+  getMonologues()
+});
+
+function getMonologues() {
+  fetch("http://localhost:3000/monologues")
+  .then(resp => resp.json())
+  .then(data => data.forEach(monologue => showMonologue(monologue)))
+}
+
+function showMonologue(monologue) {
+  const auditionRepertoire = document.getElementById("audition-repertoire")
+  const div = document.createElement("div")
+  div.classList.add("card")
+  const h2 = document.createElement("h2")
+  h2.textContent = monologue.play
+  //const img = document.createElement("img")
+  //img.src = monologue.image
+  //img.classList.add("monologue-text")
+  const p = document.createElement("p")
+  p.textContent = monologue.character
+  p.id = monologue.id
+  const button = document.createElement("button")
+  button.classList.add("like-btn")
+  button.textContent = "like"
+  button.id = monologue.id
+  div.append(h2, p, button)
+  auditionRepertoire.append(div)
+
+}
+
+function addNewMonologue(e) {
+  e.preventDefault()
+  const [play, character, scene, fullText] = e.target
+
+  fetch("http://localhost:3000/monologues", {
+    method: "POST",
+    headers: {
+      "content-type": "application/json"
+    },
+    body: JSON.stringify({
+      play: play.value,
+      character: character.value,
+      scene: scene.value,
+      fullText: fullText.value
+    })
+  })
+  .then(resp => resp.json())
+  .then(resp => showMonologue(resp))
+  const monologueFormContainer = document.querySelector(".container");
+  monologueFormContainer.reset()
+}
+
+// function updatesLikes(e) {
+//   e.preventDefault()
+//     fetch(`http://localhost:3000/monologues${e.target.id}`, {
+//       method: "PATCH",
+//       headers: {
+//         "content-type": "application/json"
+//       },
+//       body: JSON.stringify({
+//         likes: parseInt(e.target.parentElement.children[2].textContent.split(" ")[0], 10) + 1
+//       })
+//     })
+//     .then(resp => resp.json())
+//     .then(resp => {
+//       // e.target.parentElement.children[2].textContent = `${resp.likes}`
+//       const p = document.getElementById(resp.id)
+//       p.textContent = `${resp.likes} likes`
+//     })
+// }
