e6d35068fc589111afd3056bdf3a685942c03bfa
Mary Ellison Barlow <maryellisonbarlow@gmail.com>
2022-11-20 20:13:05 -0500

    Fetch delete request and click delete functional

--- src/index.js
+++ src/index.js
@@ -7,9 +7,9 @@ const FULL_STAR = '★'
 document.addEventListener("DOMContentLoaded", () => {
  const addBtn = document.querySelector("#new-monologue-btn");
  const monologueFormContainer = document.querySelector(".container");
- const form = document.querySelector(".add-monologue-form")
+ const form = document.querySelector(".add-monologue-form");
 
- form.addEventListener("submit", addNewMonologue)
+ form.addEventListener("submit", addNewMonologue);
 
  addBtn.addEventListener("click", () => {
     addMonologue = !addMonologue;
@@ -19,9 +19,12 @@ document.addEventListener("DOMContentLoaded", () => {
       monologueFormContainer.style.display = "none";
     }
   });
+
   getMonologues();
   starListener();
   heartListener();
+  removeAudition();
+
 });
 
 function getMonologues() {
@@ -34,33 +37,63 @@ function showMonologue(monologue) {
   const auditionRepertoire = document.getElementById("audition-repertoire")
   const div = document.createElement("div")
   div.classList.add("card")
+  div.id = monologue.id
+
   const h2 = document.createElement("h2")
   h2.textContent = monologue.play
+
   const p = document.createElement("p")
   p.textContent = monologue.character
   p.id = monologue.id
+
   const p2 = document.createElement("p")
   p2.textContent = monologue.fullText[[0]]
   p2.id = monologue.id
+
   const span = document.createElement("span")
   span.classList.add("star-glyph")
   span.innerHTML = EMPTY_STAR
   span.id = monologue.id
+
   const span2 = document.createElement("span")
   span2.classList.add("heart-glyph")
   span2.innerHTML = EMPTY_HEART
   span2.id = monologue.id
+
   const button = document.createElement("button")
   button.classList.add("remove-btn")
   button.id = "remove-btn"
   button.textContent = " X "
 
 
-
   div.append(h2, p, p2, span, span2, button)
   auditionRepertoire.append(div)
+}
 
 
+function removeAudition() {
+  document.addEventListener("click", (e) => {
+      if(e.target.classList[0] === 'remove-btn') {
+          e.target.parentElement.remove()
+      } 
+      else {
+        console.log("Not a Button")
+      }
+      deleteAuditionCard(e)
+    })
+}
+
+function deleteAuditionCard(e) {
+  const id = e.target.parentElement.id
+  fetch(`http://localhost:3000/monologues/${id}`,{
+    method: 'DELETE',
+    headers: {
+      "content-type": "application/json"
+
+    }
+  })
+  .then(resp => resp.json())
+  .then((monologue) => console.log(monologue))
 }
 
 function addNewMonologue(e) {
